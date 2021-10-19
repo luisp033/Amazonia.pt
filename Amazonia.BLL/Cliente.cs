2090a1eb47d0d21b23dd4892fecd17ef73877ce9
Luís Ponciano <luisp033@gmail.com>
2021-10-19 19:25:35 +0100

    dt nasc

--- Amazonia.BLL/Cliente.cs
+++ Amazonia.BLL/Cliente.cs
@@ -1,9 +1,13 @@
+using System;
+
 public class Cliente
 {
     public string Nome { get; set; }
     public Morada Morada { get; set; }
     public string Username { get; set; }
-    public string Password { get; set; }        
+    public string Password { get; set; }      
+
+    public DateTime Datanascimento {get; set;}  
 
 }
 
